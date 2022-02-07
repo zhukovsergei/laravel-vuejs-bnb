fb168f2fadb88a1cda358347ee03be229b608a8e
Zhukov Sergei <alivesystemsonly@gmail.com>
2022-02-07 03:12:52 +0100

    added availability

---
+++ app/resources/js/bookable/Availability.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h6 class="text-uppercase text-secondary fw-bolder ">Check Availability</h6>
+
+        <div class="row g-3">
+            <div class="col-md-6">
+                <label for="from" class="col-form-label">From</label>
+                <input v-model="from" type="text" class="form-control" id="from">
+            </div>
+            <div class="col-md-6">
+                <label for="to" class="col-form-label">To</label>
+                <input v-model="to" type="text" class="form-control" id="to">
+            </div>
+        </div>
+        <div class="d-grid gap-1 mt-2">
+            <button class="btn btn-secondary ">Check</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            from: null,
+            to: null,
+        }
+    },
+    created() {
+
+    },
+}
+</script>
+
+<style scoped>
+label {
+    font-size: 0.7rem;
+    text-transform: uppercase;
+    color: gray;
+    font-weight: bolder;
+}
+.is-invalid {
+    border-color: #b22222;
+    background-image: none;
+}
+.invalid-feedback {
+    color: #b22222;
+}
+</style>
